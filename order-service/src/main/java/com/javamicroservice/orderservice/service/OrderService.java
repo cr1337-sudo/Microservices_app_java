961fe488323d4dd676299c597c065cfa682da717
cr1337-sudo <cristiancuello10@gmail.com>
2024-01-28 19:38:35 -0300

    Added service discovery

--- order-service/src/main/java/com/javamicroservice/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/javamicroservice/orderservice/service/OrderService.java
@@ -20,7 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
@@ -34,8 +34,8 @@ public class OrderService {
         List<String> skuCodes = order.getOrderLineItemList().stream().map(OrderLineItems::getSkuCode).toList();
 
         // Call inventory service, and place order if product is in stock
-        InventoryResponse[] inventoryResponseArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponseArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                         uriBuilder -> uriBuilder.queryParam("skuCode", skuCodes)
                                 .build())
                 .retrieve()
